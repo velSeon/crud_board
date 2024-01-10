5d573982e27ab46775884de8919f36c05811f6b0
velSeon <seonday28@gmail.com>
2024-01-10 18:21:32 +0900

    계정 도메인 추가 작업
     회원 계정 도메인 구현 및 erd 반영 & 리팩토링: 사용 안하는 import 정리

---
+++ src/main/java/com/curd/crud_board/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.curd.crud_board.repository;
+
+import com.curd.crud_board.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
--- src/test/java/com/curd/crud_board/controller/DataRestTest.java
+++ src/test/java/com/curd/crud_board/controller/DataRestTest.java
@@ -13,10 +13,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled("Spring Data REST 통합테스트는 불필요하므로 disabled 처리")
+//@Disabled("Spring Data REST 통합테스트는 불필요하므로 disabled 처리")
 @DisplayName("Data REST 테스트")
 @Transactional
 @AutoConfigureMockMvc
@@ -85,5 +86,17 @@ public class DataRestTest {
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
+    @DisplayName("[api] 회원 관련 API 는 일체 제공하지 않는다.")
+    @Test
+    void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        // Given
 
+        // When & Then
+        mvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
+    }
 }
