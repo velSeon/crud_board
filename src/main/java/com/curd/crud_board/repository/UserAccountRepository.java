a3d06ac1fe164538e6a7e941a00da4bf0e3be071
velSeon <seonday28@gmail.com>
2024-01-15 12:09:38 +0900

    회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/curd/crud_board/repository/UserAccountRepository.java
+++ src/main/java/com/curd/crud_board/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.curd.crud_board.repository;
 import com.curd.crud_board.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
