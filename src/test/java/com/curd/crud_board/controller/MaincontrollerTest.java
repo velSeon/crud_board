7d1d7d539aa9032cf01dc8a141c5b94e31637e72
velSeon <seonday28@gmail.com>
2024-01-16 12:21:42 +0900

    문제가 된 테스트 코드 발견 및 수정
    이 부분은 진작에 개발 끝나고 테스트를 완료했던 코드인데,
    어느샌가 옛날 버전으로 돌아가 있다.
    정확한 원인은 알 수 없으나, 복구하고 테스트 완료.

--- src/test/java/com/curd/crud_board/controller/MaincontrollerTest.java
+++ src/test/java/com/curd/crud_board/controller/MaincontrollerTest.java
@@ -1,17 +1,21 @@
 package com.curd.crud_board.controller;
 
 import com.curd.crud_board.config.SecurityConfig;
+import com.curd.crud_board.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Import(SecurityConfig.class)
+@DisplayName("View 루트 컨트롤러")
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Maincontroller.class)
 class MaincontrollerTest {
 
@@ -29,7 +33,6 @@ class MaincontrollerTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("forward:/articles"))
-                .andExpect(forwardedUrl("/articles"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(forwardedUrl("/articles"));
     }
 }
