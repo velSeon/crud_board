d831a68d13c0c768b74c08a8d2b530424ec7a1a0
velSeon <seonday28@gmail.com>
2024-01-17 17:27:36 +0900

    회원 정보 서비스 로직의 구현
    회원 정보를 다루는 서비스 로직이 기존에 없었는데
    이번 기회에 간단한 형태로 구현함
    회원 정보의 조회와 저장을 담당

---
+++ src/main/java/com/curd/crud_board/service/UserAccountService.java
@@ -0,0 +1,30 @@
+package com.curd.crud_board.service;
+import com.curd.crud_board.domain.UserAccount;
+import com.curd.crud_board.dto.UserAccountDto;
+import com.curd.crud_board.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+                .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+                userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo, username))
+        );
+    }
+
+}
